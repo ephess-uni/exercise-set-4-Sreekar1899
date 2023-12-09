9896733b92a6fff585b0427d605473f79e62a88c
Gopikrishnamuthineni <gmuthine@cbu.edu>
2023-12-08 23:56:14 -0600

    submission

--- src/ex_4_0.py
+++ src/ex_4_0.py
@@ -6,14 +6,24 @@ except ImportError:
 
 # Use this FILENAME variable to test your function.
 FILENAME = get_data_file_path('messages.log')
+
+
 # >>>> DO NOT MODIFY CODE ABOVE <<<<
 
 
 def get_shutdown_events(logfile):
-    """
-    Your docstring here.  Replace the pass keyword below with your implementation
-    """
-    pass
+    with open(logfile, 'r') as file:
+
+        data = file.read()
+
+    shutdowns = list()
+
+    for line in data.splitlines():
+
+        if 'Shutdown initiated' in line:
+            shutdowns.append(line)
+
+    return shutdowns
 
 
 # >>>> The code below will call your function and print the results
--- src/ex_4_1.py
+++ src/ex_4_1.py
@@ -10,6 +10,8 @@ except ImportError:
 
 # Use this FILENAME variable to test your function.
 FILENAME = get_data_file_path('messages.log')
+
+
 # >>>> DO NOT MODIFY CODE ABOVE <<<<
 
 
@@ -17,7 +19,9 @@ def num_shutdowns(logfile):
     """
     Your docstring here.  Replace the pass keyword below with your implementation.
     """
-    pass
+    num = get_shutdown_events(logfile)
+
+    return len(num)
 
 
 # >>>> The code below will call your function and print the results
